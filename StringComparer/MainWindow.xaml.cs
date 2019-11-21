3555b7f4fd5c3938c761c87dab0b90ffe38501dd
Jeb Watson <watsonjw@DEV-WATSONJ>
2019-11-21 09:49:34 -0600

    Set a minimum width for the textboxes and clear the placeholder text on click.

--- StringComparer/MainWindow.xaml.cs
+++ StringComparer/MainWindow.xaml.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="The Watson Laboratory">
+//   Copyright © 2019 The Watson Laboratory
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace StringComparer
 {
+    using System.Windows;
+    using System.Windows.Controls;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -22,7 +16,21 @@ namespace StringComparer
     {
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+        }
+
+        private void TextBox1_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var tb = (TextBox)sender;
+            tb.Text = string.Empty;
+            tb.GotFocus -= this.TextBox1_GotFocus;
+        }
+
+        private void TextBox2_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var tb = (TextBox)sender;
+            tb.Text = string.Empty;
+            tb.GotFocus -= this.TextBox2_GotFocus;
         }
     }
 }
---
+++ StringComparer/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindowViewModel.cs" company="The Watson Laboratory">
+//   Copyright © 2019 The Watson Laboratory
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace StringComparer.ViewModel
+{
+    using System.Windows.Input;
+
+    public class MainWindowViewModel
+    {
+        public string Tb1 { get; }
+        public string Tb2 { get; }
+
+        public ICommand OnTb1MouseUpCommand { get; }
+        public ICommand OnTb2MouseUpCommand { get; }
+    }
+}
